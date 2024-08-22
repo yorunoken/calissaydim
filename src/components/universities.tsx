5f3759ece6c7bea2c5e99bbb1d121a0e6d6464dc
Yoru <m.f.fetvaci@hotmail.com>
2024-08-22 15:07:17 +0300

    finalize universities component

--- src/components/universities.tsx
+++ src/components/universities.tsx
@@ -2,7 +2,6 @@ import { BASE_URL } from "@/lib";
 import { useState, useEffect } from "react";
 import { CustomResult, ExamCategory } from "@/types/exam";
 import { University } from "@/components/university";
-import Rating from "./rating";
 import LoadingSkeleton from "./loadingSkeleton";
 
 export default function Universities({ ranking, examType }: { ranking: number; examType: ExamCategory }) {
@@ -19,16 +18,16 @@ export default function Universities({ ranking, examType }: { ranking: number; e
                     case ExamCategory.TYT:
                         console.log("fetching tyt");
                         res = await fetch(`${BASE_URL}api/${examType}?query=${query}`);
-                        console.log(res);
                         break;
                     default:
+                        console.log("fetching ayt: ", examType);
                         res = await fetch(`${BASE_URL}api/ayt/${examType}?query=${query}`);
                         break;
                 }
 
                 if (!res.ok) {
                     setError(true);
-                    console.log(`There was an error: ${res}`);
+                    console.log("There was an error: ", res);
                     return;
                 }
 
@@ -52,6 +51,21 @@ export default function Universities({ ranking, examType }: { ranking: number; e
         fetchData();
     }, [ranking, examType]);
 
+    let examTypeText = "";
+    if (examType === ExamCategory.SAY) {
+        examTypeText = "Sayısal";
+    } else if (examType === ExamCategory.SOZ) {
+        examTypeText = "Sözel";
+    } else if (examType === ExamCategory.EA) {
+        examTypeText = "Eşit ağırlık";
+    } else if (examType === ExamCategory.DIL) {
+        examTypeText = "Dil";
+    } else if (examType === ExamCategory.TYT) {
+        examTypeText = "TYT";
+    } else {
+        examTypeText = examType;
+    }
+
     if (error) {
         return <div className="mt-4 text-red-600">Veri yüklenirken hata ile karşılaşıldı.</div>;
     }
@@ -61,36 +75,35 @@ export default function Universities({ ranking, examType }: { ranking: number; e
     }
 
     if (data.length === 0) {
-        return <div className="mt-4 text-red-600">Profil verisi bulunamadı. osu!standard modunda aktif olduğunuzdan emin olun.</div>;
+        return <div className="mt-8 text-green-400">{examTypeText} sınavında oyun sıralamanız ile girebileceğiniz üniversite yok. Tebrik ederim.</div>;
     }
 
     return (
-        <div className="relative overflow-x-auto mt-8">
+        <div className="overflow-x-auto mt-8 items-center flex flex-col h-full">
+            <span className="font-extrabold text-xl uppercase pb-2">{examTypeText}</span>
             <table className="table-auto border border-slate-900 rounded w-full text-sm text-left rtl:text-right text-gray-300">
                 <thead className="text-xs uppercase bg-[#1e1e1e] text-gray-300">
                     <tr>
                         <th scope="col" className="normal-case px-3 py-2 font-bold text-xl">
                             ÜNİVERSİTE
                         </th>
-                        <th scope="col" className="normal-case px-3 py-2 font-normal text-xl">
-                            Bölüm
+                        <th scope="col" className="capitalize px-3 py-2 font-normal text-xl">
+                            bölüm
                         </th>
-                        <th scope="col" className="normal-case px-3 py-2 text-l">
-                            Sıralama 2024
+                        <th scope="col" className="capitalize px-3 py-2 text-left">
+                            sıralama 2024
                         </th>
-                        <th scope="col" className="normal-case px-3 py-2 text-l">
-                            Sıralama 2023
+                        <th scope="col" className="capitalize px-3 py-2 text-left">
+                            sıralama 2023
                         </th>
                     </tr>
                 </thead>
-                {/* tbody içine HTML constructor yazılacak */}
                 <tbody className="border-collapse">
                     {data.map((result, index) => (
                         <University key={index} university={result.universityName} className={result.className} classFaculty={result.classFaculty} tbs2023={result.tbs2023} tbs2024={result.tbs2024} />
                     ))}
                 </tbody>
             </table>
-            <Rating />
         </div>
     );
 }
--- src/components/university.tsx
+++ src/components/university.tsx
@@ -1,7 +1,7 @@
 export function University({ university, className, classFaculty, tbs2024, tbs2023 }: { university: string; className: string; classFaculty: string; tbs2024: string; tbs2023: string }) {
     return (
         <tr className="bg-zinc-800 border border-zinc-700">
-            <td className="font-normal text-l px-3 py-2 text-lg">{university}</td>
+            <td className="capitalize font-normal text-left px-3 py-2 text-lg">{university.toLocaleLowerCase("tr")}</td>
             <td className="font-normal text-base px-3 py-2">
                 {className}
                 <br />
