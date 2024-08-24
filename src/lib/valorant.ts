e77e6c5ed492a46b136cfa2e4a96c8e1df1ee525
Yoru <m.f.fetvaci@hotmail.com>
2024-08-24 19:13:10 +0300

    update valorant endpoint

--- src/lib/valorant.ts
+++ src/lib/valorant.ts
@@ -1,7 +1,7 @@
 import { userDetails } from "@/types/valorant";
 
 export async function fetchRankDetails(rank: string): Promise<userDetails | null> {
-    const res = await fetch(`/api/proxy/valorant/estimate/${rank}`);
+    const res = await fetch(`/api/proxy/estimate/valorant/${rank}`);
     if (!res.ok) {
         console.log(res);
         return null;
